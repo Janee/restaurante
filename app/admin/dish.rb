4c04fa7036cc9fab45e4f061adc8a23b8a8935c9
Diana Enriquez <diana@agaveti.com>
2015-06-02 18:57:18 -0500

    Dishes pertenecen a restaurantes

--- app/admin/dish.rb
+++ app/admin/dish.rb
@@ -1,17 +1,27 @@
 ActiveAdmin.register Dish do
+permit_params :name, :description, :price, :photo
+belongs_to :restaurante
+	
+	form do |f|	
+		f.inputs "Dish" do
+			f.input :restaurante_id, :as => :select, :collection => Restaurante.all.map{|u| [u.name, u.id]}
+	        f.input :name
+	        f.input :description
+	        f.input :price
+	        f.input :photo, :as => :file
+		end
+	    
+	    f.actions
+  	end
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
+  	show do |ad|
+      attributes_table do
+        row :name
+        row :photo do
+          image_tag(ad.photo.url(:large))
+        end
+        row :description
+        row :price
+      end
+    end
 end
--- app/admin/restaurante.rb
+++ app/admin/restaurante.rb
@@ -1,7 +1,17 @@
 ActiveAdmin.register Restaurante do
 
-permit_params :name, :description, :address, :phone,
-    dish_attributes: [:id, :name, :description, :price, :photo, :_destroy]
+	permit_params :name, :description, :address, :phone,
+    	dish_attributes: [:id, :name, :description, :price, :photo, :_destroy]
 
+    form do |f|
+    	f.inputs "Restaurante" do
+    		f.input :name
+		    f.input :description
+		    f.input :address
+		    f.input :phone
+    	end
+	    
+	   f.actions
+  	end
 
 end
